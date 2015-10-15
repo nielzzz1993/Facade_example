28e13a4aea6a107a99abd5ef47a2a118c47d68b6
Niels Carmans <niels_carmans@hotmail.com>
2015-10-15 10:59:37 +0200

    :package: Added code example

---
+++ FacadeExample/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade_example
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Facade
+            Mortgage mortgage = new Mortgage();
+
+            // Evaluate mortgage eligibility for customer
+            Customer customer = new Customer("Ann McKinsey");
+            bool eligible = mortgage.IsEligible(customer, 125000);
+
+            Console.WriteLine("\n" + customer.Name +
+                " has been " + (eligible ? "Approved" : "Rejected"));
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    internal class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassB' class
+    /// </summary>
+    internal class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassC' class
+    /// </summary>
+    internal class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Customer class
+    /// </summary>
+    internal class Customer
+    {
+        private string _name;
+
+        // Constructor
+        public Customer(string name)
+        {
+            this._name = name;
+        }
+
+        // Gets the name
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Facade' class
+    /// </summary>
+    internal class Mortgage
+    {
+        private Bank _bank = new Bank();
+        private Loan _loan = new Loan();
+        private Credit _credit = new Credit();
+
+        public bool IsEligible(Customer cust, int amount)
+        {
+            Console.WriteLine("{0} applies for {1}\u20AC loan\n",
+              cust.Name, amount);
+
+            bool eligible = true;
+
+            // Check creditworthyness of applicant
+            if (!_bank.HasSufficientSavings(cust, amount))
+            {
+                eligible = false;
+            }
+            else if (!_loan.HasNoBadLoans(cust))
+            {
+                eligible = false;
+            }
+            else if (!_credit.HasGoodCredit(cust))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+    }
+}
